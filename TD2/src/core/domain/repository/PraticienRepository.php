27743e772233ef745f32d4ea0e13cdad5846e801
Nino <nino.arcelin@gmail.com>
2024-12-10 16:24:07 +0100

    maj praticienrepo

--- TD2/src/core/domain/repository/PraticienRepository.php
+++ TD2/src/core/domain/repository/PraticienRepository.php
@@ -1,15 +1,30 @@
 <?php
 
+namespace iutnc\doctrine\core\domain\repository;
+
+
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Func;
 use iutnc\doctrine\core\domain\entities\Praticien;
+use Symfony\Component\Console\Input\ArrayInput;
 
 class PraticienRepository extends EntityRepository
 {
 
-    public function savePraticien(Praticien $praticien): Praticien
+    public function praticienBySpecialite($specialite)
     {
-        $this->getEntityManager()->persist($praticien);
-        $this->getEntityManager()->flush();
-        return $praticien;
+        $dql = "SELECT p.nom, p.prenom, p.ville, p.email, p.telephone, g.nom AS groupement_nom
+            FROM iutnc\doctrine\core\domain\entities\Praticien p
+            LEFT JOIN p.specialite s
+            LEFT JOIN p.groupement g
+            WHERE s.id = :specialiteId";
+
+        // Exécution de la requête
+        $query = $this->getEntityManager()->createQuery($dql);
+        $query->setParameter('specialiteId', $specialite);
+
+        // Retour des résultats
+        return $query->getResult();
     }
 }
