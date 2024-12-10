a8fd347cb514b9671cac58d05e98bd489e508c54
Victor Georges <victorgeorges54@gmail.com>
2024-12-10 16:43:48 +0100

    fix(ex3) : ajout de la question 2

--- TD2/src/console/ex3.php
+++ TD2/src/console/ex3.php
@@ -7,5 +7,11 @@ use iutnc\doctrine\core\domain\entities\Specialite;
 
 $specialiteRepository = $em->getRepository(Specialite::class);
 
-$specialites = $specialiteRepository->getSpecialtitesByKeyword('dentaire');
-print_r($specialites);
+
+print "Question 2 : liste des spécialités contenant un mot clé dans le libellé ou la description\n";
+$mot = 'Médecine';
+$specialites = $specialiteRepository->getSpecialtitesByKeyword($mot);
+print "Les spécialités contenant le mot clé $mot sont : \n";
+foreach ($specialites as $specialite) {    
+    print $specialite->getLibelle() . PHP_EOL;
+}
