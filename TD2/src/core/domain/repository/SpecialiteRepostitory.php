0dcc455a280252d2bd7608ae7310d2254fa29c4d
Victor Georges <victorgeorges54@gmail.com>
2024-12-10 16:23:44 +0100

    ajout de l'exo 3

---
+++ TD2/src/core/domain/repository/SpecialiteRepostitory.php
@@ -0,0 +1,17 @@
+<?php
+namespace iutnc\doctrine\core\domain\repository;
+
+use Doctrine\ORM\EntityRepository;
+use iutnc\doctrine\core\domain\repository\SpecialiteRepositoryInterface;
+
+class SpecialiteRepository extends EntityRepository implements SpecialiteRepositoryInterface
+{
+    public function getSpecialtitesByKeyword(string $keyword): array
+    {
+        $dql = 'SELECT s FROM iutnc\doctrine\core\domain\entities\Specialite s 
+                WHERE s.libelle LIKE :keyword 
+                OR s.description LIKE :keyword';
+        $query = $this->getEntityManager()->createQuery($dql);
+        return $query->getResult();
+    }
+}
